b6fdd9b547c5066b014279a03352f230638cd6bc
sarahzdev <sarah.hg.zhang@gmail.com>
2023-08-24 15:28:12 -0700

    feat: add resources

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import { StytchProvider } from '@stytch/nextjs';
 import { createStytchUIClient } from '@stytch/nextjs/ui';
 import { Albert_Sans } from 'next/font/google';
+import Image from 'next/image';
 
 const { provider, chains } = configureChains(
   [mainnet, goerli, optimism],
@@ -43,9 +44,30 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StytchProvider stytch={stytch}>
       <WagmiConfig client={client}>
+        <header>
+          <Image src="/lit.svg" alt="Lit logo" width={32} height={32}></Image>
+          <a
+            href="https://developer.litprotocol.com/?ref=demo.getlit.dev"
+            target="_blank"
+            rel="noopener nofollow"
+            className="lit-cta"
+          >
+            Build on Lit
+          </a>
+        </header>
         <main className={font.className}>
           <Component {...pageProps} />
         </main>
+        <footer>
+          <a
+            href="https://github.com/LIT-Protocol/pkp-social-auth-example"
+            target="_blank"
+            rel="noopener nofollow"
+            className="footer-link"
+          >
+            View the source code
+          </a>
+        </footer>
       </WagmiConfig>
     </StytchProvider>
   );
